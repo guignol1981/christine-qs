ead5c07e06c35bd1cf2909d124ae023b1a36fd20
Etienne Matteau <ematteau@jstrategia.com>
2022-08-20 08:31:14 -0400

    Modification titre et SEO

--- src/App.vue
+++ src/App.vue
@@ -16,17 +16,17 @@ export default {
     },
     setup() {
         useHead({
-            title: 'Christine Gilbert - QS',
+            title: 'Christine Gilbert | Québec solidaire',
             meta: [
                 {
                     name: 'description',
                     content: 'Christine Gilbert est la candidate de Québec solidaire dans Lotbinière-Frontenac',
                 },
-                { property: 'og:title', content: "Christine Gilbert - Québec solidaire"},
+                { property: 'og:title', content: "Christine Gilbert | Québec solidaire"},
                 {
                   property: 'og:description',
                   content: "Christine Gilbert est la candidate de Québec solidaire dans Lotbinière-Frontenac pour les élections provinciales de 2022." +
-                    "Christine habite à Saint-Antoine-de-Tilly et elle a grandi dans la région de Thetford Mines."
+                    "Christine a un fort sentiment d’appartenance à la circonscription Lotbinière-Frontenac, autant par son lieu d’habitation qui se situe dans Lotbinière, que par la région d’origine de sa famille, Thetford Mines, dans Frontenac."
                 },
                 { property: 'og:image', content: "https://www.lepeuplelotbiniere.ca/images/contenu/plus/114464_465_310.jpg" },
                 { property: 'og:site_name', content: 'Christine Gilbert - Québec solidaire' },
