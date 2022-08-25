54f2a0a3274133b6a328d9eea01239d9f8f334fc
Etienne Matteau <ematteau@jstrategia.com>
2022-08-25 18:03:59 -0400

    Ajustements width des sections

--- src/components/ProfilePicture.vue
+++ src/components/ProfilePicture.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="lg:w-2/3 relative">
+    <section class="lg:w-2/3 2xl:w-1/2 relative">
         <img src="@/assets/Profile_ChristineGilbert.jpg" alt="Photo profil" />
         <div class="absolute text-5xl md:text-8xl lg:text-6xl xl:text-7xl font-semibold text-white top-0 left-0 mx-3 my-1 lg:mt-8"><h1>Christine</h1><h1>Gilbert</h1></div>
         <div class="absolute text-5xl md:text-8xl lg:text-6xl xl:text-7xl font-semibold text-white bottom-0 right-0 lg:top-0 m-3 lg:mx-3 lg:my-1 lg:mt-8"><h1>Lotbinière-</h1><h1>Frontenac</h1></div>
